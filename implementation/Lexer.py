ccdfac3ef550a00bf2779f8714bed1d21b32348c
Galitskiy Kirill <int33h@mail.ru>
2023-05-20 18:07:07 +0300

    Checkers added (pylint, mypy, flake8)

--- implementation/Lexer.py
+++ implementation/Lexer.py
@@ -1,39 +1,48 @@
+"""Lexer implementation"""
 import unittest
 import os
-from Token import *
+from Token import Token
+from Token import TokenPlus, TokenMinus
+from Token import TokenLParenthese, TokenRParenthese
+
 
 class Lexer:
+    """Lexer class"""
     def __init__(self, string: str):
-        self.file     = string
-        self.line     = 1
-        self.column   = 0
-        self.position = 0
-    
+        self.file     = string  # noqa: E221
+        self.line     = 1  # noqa: E221
+        self.column   = 0  # noqa: E221
+        self.position = 0  # noqa: E221
+
     def PeekChar(self, offset: int = 0) -> str:
+        """returns char at current position + offset or '\0'"""
         if self.position + offset >= len(self.file):
             return "\0"
         return self.file[self.position + offset]
-    
+
     def NextChar(self) -> str:
+        """returns next char or '\0'"""
         if self.PeekChar() == '\n':
-            self.line   += 1
-            self.column =  0
+            self.line   += 1  # noqa: E221, E222
+            self.column =  0  # noqa: E221, E222
         else:
-            self.column += 1
-        
+            self.column += 1  # noqa: E221, E222
+
         self.position += 1
         return self.PeekChar()
-    
+
     def TokenizeNumber(self):
+        """scans string for number"""
         number = 0
-        char   = self.PeekChar()
+        char   = self.PeekChar()  # noqa: E221
         while char.isdigit():
             number = number * 10 + int(char)
-            char   = self.NextChar()
-        
+            char   = self.NextChar()  # noqa: E221
+
         return number
-    
-    def NextToken(self):
+
+    def NextToken(self):  # pylint: disable=R0911
+        """returns next token from string"""
         char = self.PeekChar()
         while char.isspace():
             char = self.NextChar()
@@ -42,10 +51,10 @@ class Lexer:
             return None
 
         # pre save
-        line   = self.line
-        column = self.column
+        line   = self.line    # noqa: E221
+        column = self.column  # noqa: E221
         if char.isdigit():
-            data   = self.TokenizeNumber()
+            data = self.TokenizeNumber()
             return Token("Number", data, line, column)
 
         if char == '+':
@@ -65,6 +74,7 @@ class Lexer:
         return None
 
     def Tokens(self):
+        """returns list of tokens for string"""
         result = []
         while True:
             token = self.NextToken()
@@ -73,27 +83,31 @@ class Lexer:
             result.append(token)
         return result
 
+
 class LexerTestCase(unittest.TestCase):
+    """Lexer test class"""
     def __init__(self, testName, path):
-        super(LexerTestCase, self).__init__(testName)
+        super().__init__(testName)
         self.path = path
 
-    def test_file(self):
+    def TestFile(self):
+        """do a test from file"""
         print(self.path)
-        with open(self.path, "r") as file:
+        with open(self.path, encoding="utf-8") as file:
             lines = file.readlines()
             self.assertEqual(lines[0], "input:\n")
             self.assertEqual(lines[2], "output:\n")
-            
-            lexer  = Lexer(lines[1])
+
+            lexer  = Lexer(lines[1])  # noqa: E221
             result = lexer.Tokens()
             self.assertEqual(str(result), lines[3][:-1])
+
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     with os.scandir("../tests/lexer") as iterator:
         for entity in iterator:
-            if entity.is_file():    
-                suite.addTest(LexerTestCase("test_file", entity.path))
-    
+            if entity.is_file():
+                suite.addTest(LexerTestCase("TestFile", entity.path))
+
     unittest.TextTestRunner(verbosity=2).run(suite)
--- implementation/Token.py
+++ implementation/Token.py
@@ -1,40 +1,69 @@
+"""Token implementation"""
+
+
 class Token:
-    def __init__(self, type: str, data: int | None=None, line=None, column=None):
-        self.type   = type
-        self.data   = data
-        self.line   = line
+    """Input element for parser"""
+    def __init__(self,
+                 type:   str,  # pylint: disable=W0622
+                 data:   int | None = None,
+                 line:   int | None = None,
+                 column: int | None = None):
+        self.type   = type  # noqa: E221
+        self.data   = data  # noqa: E221
+        self.line   = line  # noqa: E221
         self.column = column
-    
+
     def __str__(self):
         return repr(self)
-    
+
     def __repr__(self):
-        if self.data is not None:
-            return f"{self.type}({self.data})"
-        else:
+        if self.data is None:
             return f"{self.type}"
-    
+
+        return f"{self.type}({self.data})"
+
     def __eq__(self, other):
-        if type(other) == str:
+        if isinstance(other, str):
             return self.type == other
-        
+
         if isinstance(other, Token):
             return (self.type == other.type) and (self.data == other.data)
 
-class TokenPlus(Token):
-    def __init__(self, line: int = None, column: int = None) -> None:
+        return False
+
+
+class TokenPlus(Token):  # pylint: disable=R0903
+    """Operator '+' token"""
+    def __init__(self,
+                 line: int | None = None,
+                 column: int | None = None):
         super().__init__("Plus", None, line, column)
-class TokenMinus(Token):
-    def __init__(self, line: int = None, column: int = None) -> None:
+
+
+class TokenMinus(Token):  # pylint: disable=R0903
+    """Operator '-' token"""
+    def __init__(self,
+                 line: int | None = None,
+                 column: int | None = None):
         super().__init__("Minus", None, line, column)
 
-class TokenLParenthese(Token):
-    def __init__(self, line: int = None, column: int = None) -> None:
+
+class TokenLParenthese(Token):  # pylint: disable=R0903
+    """'(' token"""
+    def __init__(self,
+                 line: int | None = None,
+                 column: int | None = None):
         super().__init__("LParenthese", None, line, column)
-class TokenRParenthese(Token):
-    def __init__(self, line: int = None, column: int = None) -> None:
+
+
+class TokenRParenthese(Token):  # pylint: disable=R0903
+    """')' token"""
+    def __init__(self,
+                 line: int | None = None,
+                 column: int | None = None):
         super().__init__("RParenthese", None, line, column)
-        
+
+
 if __name__ == "__main__":
     token1 = Token("Plus")
     token2 = Token("Plus")
